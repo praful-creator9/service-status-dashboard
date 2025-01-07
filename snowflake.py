42b1a18de7c26456d017013e52d3fb05311dd466
praful-creator9 <digitalhome9861@gmail.com>
2025-01-07 23:56:32 +0530

    Create snowflake.py

---
+++ snowflake.py
@@ -0,0 +1,13 @@
+import requests
+
+def fetch_snowflake_status():
+    url = "https://status.snowflake.com/api/v2/status.json"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return data['status']['description'], data['status']['indicator']
+        else:
+            return "Failed to fetch", "red"
+    except Exception as e:
+        return f"Error: {str(e)}", "red"
